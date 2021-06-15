a5e52e656b0b0bbb1781029dd159ede9c88a8c2e
Samuel D N Johnson <samueldnjohnson@gmail.com>
2021-06-15 15:30:17 -0700

    Pushing to MP

--- tuningAnalysis/runMPs_F01.R
+++ tuningAnalysis/runMPs_F01.R
@@ -14,10 +14,12 @@ source("initTest.R")
 source("runCMPs.R")
 source("makeGridMPs_F01.R")
 
-makeGridFzero1( qGrid = seq(from = 0.3, to = 0.6, by = 0.1),
+makeGridFzero1( qGrid = seq(from = 0.2, to = 0.8, by = 0.1),
                 outFile = "autoF01gridMPs.R")
 
 source("autoF01gridMPs.R")
+
+projFolder <- "testF01_qGrid_allOMs"
 
 # sfInit(parallel=TRUE, cpus = 2 )
 # sfLibrary( ABTMSE )
@@ -28,14 +30,35 @@ source("autoF01gridMPs.R")
 # sfSource("plots.R")
                   
 
-OMdvec <- c(1:3)
+OMdvec <- c(1:48)
 
 gridqMSEs <- lapply(  X = OMdvec, 
                       FUN = runCMPs,
                       MPs = gridMPs,
                       checkMPs = TRUE,
                       reloadABT = FALSE,
-                      projFolderName = "testF01_qGrid_OM1to3" )
+                      projFolderName = projFolder )
+
+
+
+
+pH30_E <- lapply(X = gridqMSEs, FUN = pH30, pp = 1)
+pH30_W <- lapply(X = gridqMSEs, FUN = pH30, pp = 2)
+
+PGK_E <- lapply(X = gridqMSEs, FUN = PGK, pp = 1)
+PGK_W <- lapply(X = gridqMSEs, FUN = PGK, pp = 2)
+
+yrHealth_E <- lapply(X = gridqMSEs, FUN = tfHealthy_t, pp = 1)
+yrHealth_W <- lapply(X = gridqMSEs, FUN = tfHealthy_t, pp = 2)
+
+perfMetricList <- list( pH30_E = pH30_E,
+                        pH30_W = pH30_W,
+                        PGK_E = PGK_E,
+                        PGK_W = PGK_W,
+                        yrHealth_E = yrHealth_E,
+                        yrHealth_W = yrHealth_W )
+
+saveRDS(perfMetricList, file = file.path("MSEs",projFolder,"PMlist.rds"))
 
 #z <- runCMPs("OM_87d",noCapMPs,checkMPs=1,projFolderName="sep12")
 
